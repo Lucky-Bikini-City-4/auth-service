d7f159ba100ffb03f78810a9305f8d11f27e5ea6
legoChoi <chlx159@gmail.com>
2025-09-20 20:45:47 +0900

    fix: 소셜 로그인 플래그 값에 따라 응답 message 변경되도록 수정

--- src/main/java/com/dayaeyak/auth/domain/auth/GoogleController.java
+++ src/main/java/com/dayaeyak/auth/domain/auth/GoogleController.java
@@ -4,6 +4,7 @@ import com.dayaeyak.auth.common.constant.AuthResponseMessage;
 import com.dayaeyak.auth.common.entity.ApiResponse;
 import com.dayaeyak.auth.domain.auth.dto.response.AuthSocialLoginResponseDto;
 import com.dayaeyak.auth.domain.auth.enums.ProviderType;
+import com.dayaeyak.auth.domain.auth.enums.SocialLoginFlag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,10 @@ public class GoogleController {
             @RequestParam String code
     ) {
         AuthSocialLoginResponseDto data = oAuthService.processSocialLogin(ProviderType.GOOGLE, code);
+
+        if (data.flag().equals(SocialLoginFlag.JOIN_REQUIRED)) {
+            return ApiResponse.success(HttpStatus.OK, AuthResponseMessage.NEED_SIGNUP_BY_SOCIAL, data);
+        }
 
         return ApiResponse.success(HttpStatus.OK, AuthResponseMessage.GOOGLE_LOGIN, data);
     }
--- src/main/java/com/dayaeyak/auth/domain/auth/KakaoController.java
+++ src/main/java/com/dayaeyak/auth/domain/auth/KakaoController.java
@@ -4,6 +4,7 @@ import com.dayaeyak.auth.common.constant.AuthResponseMessage;
 import com.dayaeyak.auth.common.entity.ApiResponse;
 import com.dayaeyak.auth.domain.auth.dto.response.AuthSocialLoginResponseDto;
 import com.dayaeyak.auth.domain.auth.enums.ProviderType;
+import com.dayaeyak.auth.domain.auth.enums.SocialLoginFlag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,10 @@ public class KakaoController {
             @RequestParam String code
     ) {
         AuthSocialLoginResponseDto data = oAuthService.processSocialLogin(ProviderType.KAKAO, code);
+
+        if (data.flag().equals(SocialLoginFlag.JOIN_REQUIRED)) {
+            return ApiResponse.success(HttpStatus.OK, AuthResponseMessage.NEED_SIGNUP_BY_SOCIAL, data);
+        }
 
         return ApiResponse.success(HttpStatus.OK, AuthResponseMessage.KAKAO_LOGIN, data);
     }
