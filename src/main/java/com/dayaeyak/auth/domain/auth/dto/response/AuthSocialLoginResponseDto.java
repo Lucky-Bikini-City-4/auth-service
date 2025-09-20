11fe2d995e6e755b6281622bdce19ece937513cb
legoChoi <chlx159@gmail.com>
2025-09-20 14:15:20 +0900

    feat: 소셜 로그인 응답 값 ProviderType 추가

--- src/main/java/com/dayaeyak/auth/domain/auth/dto/response/AuthSocialLoginResponseDto.java
+++ src/main/java/com/dayaeyak/auth/domain/auth/dto/response/AuthSocialLoginResponseDto.java
@@ -1,11 +1,14 @@
 package com.dayaeyak.auth.domain.auth.dto.response;
 
+import com.dayaeyak.auth.domain.auth.enums.ProviderType;
 import com.dayaeyak.auth.domain.auth.enums.SocialLoginFlag;
 import lombok.Builder;
 
 @Builder
 public record AuthSocialLoginResponseDto(
         SocialLoginFlag flag,
+
+        ProviderType providerType,
 
         String accessToken,
 
@@ -14,17 +17,19 @@ public record AuthSocialLoginResponseDto(
         String tempToken
 ) {
 
-    public static AuthSocialLoginResponseDto success(String accessToken, String refreshToken) {
+    public static AuthSocialLoginResponseDto success(ProviderType providerType, String accessToken, String refreshToken) {
         return AuthSocialLoginResponseDto.builder()
                 .flag(SocialLoginFlag.SUCCESS)
+                .providerType(providerType)
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
     }
 
-    public static AuthSocialLoginResponseDto joinRequired(String tempToken) {
+    public static AuthSocialLoginResponseDto joinRequired(ProviderType providerType, String tempToken) {
         return AuthSocialLoginResponseDto.builder()
                 .flag(SocialLoginFlag.JOIN_REQUIRED)
+                .providerType(providerType)
                 .tempToken(tempToken)
                 .build();
     }
