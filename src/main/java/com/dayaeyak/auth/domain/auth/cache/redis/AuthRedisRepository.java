7472b6069613dff62c254465c5534deacff76974
legoChoi <chlx159@gmail.com>
2025-09-20 13:23:51 +0900

    feat: 소셜 로그인 시 추가 정보 입력을 통한 회원가입 시 임시 저장할 소셜 유저 정보 모델 추가

--- src/main/java/com/dayaeyak/auth/domain/auth/cache/redis/AuthRedisRepository.java
+++ src/main/java/com/dayaeyak/auth/domain/auth/cache/redis/AuthRedisRepository.java
@@ -1,5 +1,7 @@
 package com.dayaeyak.auth.domain.auth.cache.redis;
 
+import com.dayaeyak.auth.domain.auth.cache.redis.model.TempSocialUserInfo;
+
 import java.util.Optional;
 
 public interface AuthRedisRepository {
@@ -9,4 +11,8 @@ public interface AuthRedisRepository {
     Optional<Long> findAndDeleteByRefreshToken(String refreshToken);
 
     boolean deleteByRefreshToken(String refreshToken);
+
+    void saveSocialUserInfo(String tempToken, TempSocialUserInfo tempSocialUserInfo);
+
+    void deleteSocialUserInfo(String tempToken);
 }
